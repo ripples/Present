10d7a93005d6849be4ddcfc6fe6addc381dc8bc2
Rajesh Susai <rajeshsusai14@gmail.com>
2017-10-25 11:39:56 -0400

    auth for image sync, sorta

--- server/routes/index.js
+++ server/routes/index.js
@@ -112,36 +112,38 @@ Scheme for sourceID
 Maybe some diffing... 
 */
 router.get('/image/:courseId/:lectureName/:sourceId/:time', function (req, res) {
-	const feedType = (req.params["sourceId"].split("-")[0] === "1") ? "computer" : "whiteBoard"
-	const feedId = req.params["sourceId"].split("-")[1]
-	const fpath = "./lectures/" + req.params.courseId.toString() + '/' + req.params.lectureName.toString()
-	util.promisify(fs.readFile)(fpath+ '/INFO', 'utf8').then( contents =>{
-		const re = /(?:timestamp: (\d*))/
-		const found = contents.match(re)[1];
-		return parseInt(req.params.time) + parseInt(found)
-	}).then( cTime => {
-		util.promisify(fs.readdir)(fpath + '/' + feedType).then( files => {
-			const fileName = files.reduce((result, file) => {
-				const splitFileName = file.split('-')
-				const fileTime = parseInt(splitFileName[2].split('.')[0])
-				if(splitFileName[0] === feedType && splitFileName[1] === feedId && fileTime <= cTime){
-					result.push({
-						name: file,
-						time: fileTime
-					})
+	if(courseId == req.params.courseId) {
+		const feedType = (req.params["sourceId"].split("-")[0] === "1") ? "computer" : "whiteBoard"
+		const feedId = req.params["sourceId"].split("-")[1]
+		const fpath = "./lectures/" + req.params.courseId.toString() + '/' + req.params.lectureName.toString()
+		util.promisify(fs.readFile)(fpath+ '/INFO', 'utf8').then( contents =>{
+			const re = /(?:timestamp: (\d*))/
+			const found = contents.match(re)[1];
+			return parseInt(req.params.time) + parseInt(found)
+		}).then( cTime => {
+			util.promisify(fs.readdir)(fpath + '/' + feedType).then( files => {
+				const fileName = files.reduce((result, file) => {
+					const splitFileName = file.split('-')
+					const fileTime = parseInt(splitFileName[2].split('.')[0])
+					if(splitFileName[0] === feedType && splitFileName[1] === feedId && fileTime <= cTime){
+						result.push({
+							name: file,
+							time: fileTime
+						})
+					}
+					return result
+				}, []).sort((left, right) => left.time - right.time).pop() //this should be the file
+				if(typeof fileName != 'undefined' && fileName != null){
+					res.sendFile(path.resolve('lectures', req.params.courseId.toString(), req.params.lectureName.toString(), feedType, fileName.name))
 				}
-				return result
-			}, []).sort((left, right) => left.time - right.time).pop() //this should be the file
-			if(typeof fileName != 'undefined' && fileName != null){
-				res.sendFile(path.resolve('lectures', req.params.courseId.toString(), req.params.lectureName.toString(), feedType, fileName.name))
-			}
-			else{
-				res.status(404).send()
-			}
-		}).catch( err => res.status(404).send(err))
-	}).catch( err => {
-		res.status(404).send(err)
-	})
+				else{
+					res.status(404).send()
+				}
+			}).catch( err => res.status(404).send(err))
+		}).catch( err => {
+			res.status(404).send(err)
+		})
+	}
 });
 
 module.exports = router;
