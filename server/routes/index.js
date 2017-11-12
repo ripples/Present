931868f2a17104c7154a772c21e7df1b26379cc3
Ryan Jerue <ryan@jerue.org>
2017-11-12 09:11:44 -0500

    formatting

--- server/routes/index.js
+++ server/routes/index.js
@@ -16,79 +16,79 @@ router.post('/data', function (req, res) {
 });
 
 router.get('/identify/*', function (req, res) {
-		const tok = req.params[0].replace(/-/g, '/');
-		const unhashed = encryptor.decrypt(tok);
-		if (typeof unhashed == 'undefined' || unhashed === null) {
-			res.status(404).send('Not Found');
-		}
-		else {
-			appGetAuth = false;
-			res.send(unhashed);
-		}
+	const tok = req.params[0].replace(/-/g, '/');
+	const unhashed = encryptor.decrypt(tok);
+	if (typeof unhashed == 'undefined' || unhashed === null) {
+		res.status(404).send('Not Found');
+	}
+	else {
+		appGetAuth = false;
+		res.send(unhashed);
+	}
 });
 
 router.get('/listOfCourseLectures/:courseId', function (req, res) {
-		dirToJson("./lectures/" + req.params.courseId.toString(), function (err, dirTree) {
-			if (err) {
-				throw err;
-			} else {
-				appGetLectures = false;
-				res.send(dirTree);
-			}
-		});
+	dirToJson("./lectures/" + req.params.courseId.toString(), function (err, dirTree) {
+		if (err) {
+			throw err;
+		} else {
+			appGetLectures = false;
+			res.send(dirTree);
+		}
+	});
 });
 
 router.get('/manifest/:courseId/:lectureName', function (req, res) {
-		const fpath = "./lectures/" + req.params.courseId.toString() + '/' + req.params.lectureName.toString() + '/INFO'
-		fs.readFile(fpath, 'utf8', function (err, contents) {
-			if (err) {
-				res.status(404).send('Not Found');
-				return;
+	const fpath = "./lectures/" + req.params.courseId.toString() + '/' + req.params.lectureName.toString() + '/INFO'
+	fs.readFile(fpath, 'utf8', function (err, contents) {
+		if (err) {
+			res.status(404).send('Not Found');
+			return;
+		}
+		else {
+			const re = /(?:whiteboardCount: (\d))(?:\s|.*)*(?:computerCount: (\d))/ //this is a little bit more delicate than I'd like it to be
+			const found = contents.match(re)
+			const manifest = {
+				whiteboardCount: parseInt(found[1]), //for some reason there is a third capture group at 0...
+				computerCount: parseInt(found[2]),
+				input: found['input']
 			}
-			else {
-				const re = /(?:whiteboardCount: (\d))(?:\s|.*)*(?:computerCount: (\d))/ //this is a little bit more delicate than I'd like it to be
-				const found = contents.match(re)
-				const manifest = {
-					whiteboardCount: parseInt(found[1]), //for some reason there is a third capture group at 0...
-					computerCount: parseInt(found[2]),
-					input: found['input']
-				}
-				res.send(manifest)
-			}
-		})
+			res.send(manifest)
+		}
+	})
 })
 
 
 router.get('/video/:courseId/:lectureName', function (req, res) {
-		const fpath = "./lectures/" + req.params.courseId.toString() + '/' + req.params.lectureName.toString() + '/videoLarge.mp4'  // TODO tie this to absolute location
-		const stat = fs.statSync(fpath)
-		const fileSize = stat.size
-		const range = req.headers.range
-		if (range) {
-			const parts = range.replace(/bytes=/, "").split("-")
-			const start = parseInt(parts[0], 10)
-			const end = parts[1]
-				? parseInt(parts[1], 10)
-				: fileSize - 1
-			const chunksize = (end - start) + 1
-			const file = fs.createReadStream(fpath, { start, end })
-			const head = {
-				'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-				'Accept-Ranges': 'bytes',
-				'Content-Length': chunksize,
-				'Content-Type': 'video/mp4',
-			}
-			res.writeHead(206, head);
-			file.pipe(res);
+	const fpath = "./lectures/" + req.params.courseId.toString() + '/' + req.params.lectureName.toString() + '/videoLarge.mp4'  // TODO tie this to absolute location
+	const stat = fs.statSync(fpath)
+	const fileSize = stat.size
+	const range = req.headers.range
+	if (range) {
+		const parts = range.replace(/bytes=/, "").split("-")
+		const start = parseInt(parts[0], 10)
+		const end = parts[1]
+			? parseInt(parts[1], 10)
+			: fileSize - 1
+		const chunksize = (end - start) + 1
+		const file = fs.createReadStream(fpath, { start, end })
+		const head = {
+			'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+			'Accept-Ranges': 'bytes',
+			'Content-Length': chunksize,
+			'Content-Type': 'video/mp4',
 		}
-		else {
-			const head = {
-				'Content-Length': fileSize,
-				'Content-Type': 'video/mp4',
-			}
-			res.writeHead(200, head)
-			fs.createReadStream(fpath).pipe(res)
+		res.writeHead(206, head);
+		file.pipe(res);
+	}
+	else {
+		const head = {
+			'Content-Length': fileSize,
+			'Content-Type': 'video/mp4',
 		}
+		res.writeHead(200, head)
+		fs.createReadStream(fpath).pipe(res)
+	}
 });
 
 
