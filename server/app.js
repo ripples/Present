55c697efb11b8475438c4b8b2a46110c978a1dfb
Ryan Jerue <ryan@jerue.org>
2017-11-12 06:22:09 -0500

    conversion from body parser to busboy

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bb = require('express-busboy');
 var session = require('express-session');
 
 var index = require('./routes/index');
@@ -16,11 +16,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json({limit: "5000mb", parameterLimit: 1000000}));
-//app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({limit: '5000mb', extended: true, parameterLimit: 1000000}));
+
+bb.extend(app, {
+    upload: true,
+    path: './uploads',
+    allowedPath: /./
+});
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
@@ -34,6 +37,7 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use('/', index);
 
 // catch 404 and forward to error handler
