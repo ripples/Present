843069888a81e6c33805112c2b59ad499e88bb36
Ryan Jerue <ryan@jerue.org>
2017-11-12 06:06:04 -0500

    changes to express config to allow large files

--- server/app.js
+++ server/app.js
@@ -18,8 +18,9 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: "5000mb", parameterLimit: 1000000}));
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({limit: '5000mb', extended: true, parameterLimit: 1000000}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
