b6f53b11bffa399ec143c0cf3eb05ce0ffe08e33
Ryan Jerue <ryan@jerue.org>
2017-11-12 20:07:07 -0500

    more changes

---
+++ server/routes/upload.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path')
+const fs = require('fs')
+const unzip = require('unzip')
+
+router.post('/upload/lecture-zip', function (req, res) {
+	const file = req.files.file
+	const tmp_path = file.file;
+	const target_path = './lectures';
+	try {
+		fs.createReadStream(tmp_path).pipe(unzip.Extract({ path: target_path }));
+		res.status(200).send()
+	}
+	catch (err) {
+		res.status(400).send(err)
+	}
+
+	//now cleanup
+	fs.unlink(tmp_path, (err) => {
+		if (err) console.log("Error deleting file \"" + tmp_path + "\", consider removing files in tmp upload directory \nERR: " + err)
+	})
+});
+
+router.post('/upload/:courseId/lecture-zip', function (req, res) {
+	const file = req.files.file
+	const courseId = req.params.courseId
+	const tmp_path = file.file;
+	const target_path = './lectures/' + courseId;
+	try {
+		fs.createReadStream(tmp_path).pipe(unzip.Extract({ path: target_path }));
+		res.status(200).send()
+	}
+	catch (err) {
+		res.status(400).send(err)
+	}
+
+	//now cleanup
+	fs.unlink(tmp_path, (err) => {
+		if (err) console.log("Error deleting file \"" + tmp_path + "\", consider removing files in tmp upload directory \nERR: " + err)
+	})
+});
+
+module.exports = router;
