77a20ac325be83d4541393e0befad4d9f34a7ac7
Ryan Jerue <ryan@jerue.org>
2017-11-14 02:51:05 -0500

    some more npm scripts

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -3,4 +3,5 @@ var proxy = require('redbird')({port: 80});
 proxy.register("localhost", "http://localhost:3000");
 
 proxy.register("localhost/api", "http://localhost:3001/api");
+
 proxy.register("localhost/upload", "http://localhost:3001/upload");
