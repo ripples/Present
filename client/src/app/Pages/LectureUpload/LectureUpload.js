e049aedacc0e52f4b133c318b8ac072be7fcd3cd
lrothschildshea <lrothschildshea@gmail.com>
2017-11-16 15:39:47 -0500

    minor changes

--- client/src/app/Pages/LectureUpload/LectureUpload.js
+++ client/src/app/Pages/LectureUpload/LectureUpload.js
@@ -16,9 +16,9 @@ export default class LectureUpload extends React.Component {
                 <div className="col-md-6">
                     <h1>Lecture Upload</h1>
                     <form>
-                        <h4>Please select a file to upload</h4>                        
-                        <input type="file" name="usrname" /> 
-                        <input type="button" value="Submit" onClick={this.uploadVideo} />
+                        <h4>Please select a file to upload</h4>
+                        <input type="file" accept="video/mp4" name="lectureVideo" />
+                        <input type="submit" value="Submit" onClick={this.uploadVideo} />
                     </form>
                 </div>
                 <div className="col-md-3">
