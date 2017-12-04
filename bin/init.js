3b3736852fe8ff36139a72faf54b88796788beff
Ryan Jerue <ryan@jerue.org>
2017-12-04 17:21:09 -0500

    changes to port # to prevent elevation

--- bin/init.js
+++ bin/init.js
@@ -8,5 +8,5 @@ process.env.SERVER_PATH = (process.env.SERVER_PATH) ? process.env.SERVER_PATH : 
 process.env.SERVER_PORT = (process.env.SERVER_PORT) ? process.env.SERVER_PORT : 3001
 process.env.PRESENT_PATH = (process.env.PRESENT_PATH) ? process.env.PRESENT_PATH : 'localhost' 
 process.env.PRESENT_PORT = (process.env.PRESENT_PORT) ? process.env.PRESENT_PORT : 3000 
-process.env.PROXY_PORT = (process.env.PROXY_PORT) ? process.env.PROXY_PORT : 80
+process.env.PROXY_PORT = (process.env.PROXY_PORT) ? process.env.PROXY_PORT : 3002
 process.env.SECRET = (process.env.SECRET) ? process.env.SECRET : "It never rains in Southern California" 
