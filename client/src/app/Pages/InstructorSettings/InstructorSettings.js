8b4e0c86f48afb542aebcdc885827dab8f26f834
Rajesh Susai <rajeshsusai14@gmail.com>
2017-12-13 00:36:23 -0500

    added hamburger menu for Instructor Settings. Need to add styling to completely hide menu when initially entering page

--- client/src/app/Pages/InstructorSettings/InstructorSettings.js
+++ client/src/app/Pages/InstructorSettings/InstructorSettings.js
@@ -4,6 +4,7 @@ import CalendarRobust from '../../components/CalendarForm/CalendarRobust';
 import LectureUpload from '../../components/LectureUpload/LectureUpload';
 import LectureDelete from '../../components/LectureDelete/LectureDelete';
 import {setInstructorPage, clearInstructorPage} from '../../Actions/instructorSettingsActions.js';
+import { slide as Menu} from 'react-burger-menu';
 
 class InstructorSettings extends Component {
 
@@ -32,6 +33,10 @@ class InstructorSettings extends Component {
         this.props.clearInstructorPage();
     }
 
+    showSettings (event) {
+        event.preventDefault();
+    }
+
     render(){
 
         return(
@@ -39,21 +44,17 @@ class InstructorSettings extends Component {
                 { ((typeof(this.props.roles) !== "undefined" && this.props.roles.toLowerCase().includes("instructor")) ?
                 <div className="row" >
                     <div className="col-md-12">
-                        <h1 style = {headerStyle} >Settings</h1>
-                        <div className="col-md-4">
-                            <div>
-                                <button style={buttonStyle} onClick={this.onClick.bind(this, "lectureUpload")}><div style = {textStyle}>Lecture Upload</div></button>
-                            </div>
-                            <div>
-                                <button style={buttonStyle} onClick={this.onClick.bind(this, "lectureDelete")}><div style = {textStyle}>Lecture Delete</div></button>
-                            </div>
-                            <div>
-                                <button style={buttonStyle} onClick={this.onClick.bind(this, "calendar")}><div style = {textStyle}>Calendar</div></button>
-                            </div>
+                        <Menu noOverlay styles={styles}>
+                                <a style={buttonStyle} className="menu-item" onClick={this.onClick.bind(this, "lectureUpload")}><div style = {textStyle}>Lecture Upload</div></a>
+                                <a style={buttonStyle} className="menu-item" onClick={this.onClick.bind(this, "lectureDelete")}><div style = {textStyle}>Lecture Delete</div></a>
+                                <a style={buttonStyle} className="menu-item" onClick={this.onClick.bind(this, "calendar")}><div style = {textStyle}>Calendar</div></a>
+                        </Menu>
+                        <div className="col-md-2">
                         </div>
                         <div className="col-md-8" >
                             {this.renderComponent(this.props.instructorPage)}
                         </div>
+                        <div className="col-md-2" />
                     </div>
                 </div>
                 :
@@ -72,7 +73,6 @@ const mapStateToProps = state => {
         courseId: state.token.lis_course_section_sourcedid,
         instructorPage: state.instructorPage.page,
         roles: state.token.roles,
-        ipage: state.instructorPage
     };
 };
 
@@ -84,26 +84,56 @@ const mapDispatchToProps = (dispatch, ownProps) => {
 };
 
 var buttonStyle= {
-    backgroundColor: "white",
-    borderRadius: "4px",
     marginTop: "20px",
-    color: "black",
     paddingLeft: "10px",
     paddingRight: "10px",
     paddingTop: "4px",
     paddingBottom: "4px",
-    border: "solid black 1px",
-    outline: "none"
+    cursor: "pointer"
 }
 
 var headerStyle= {
 	fontWeight: "bold",
-    fontSize: "36px",
-    marginBottom: "40px"
+    fontSize: "36px"
 }
 
 var textStyle = {
-    fontSize: "20px"
+    fontSize: "20px",
+    color: "white"
 }
 
+var styles = {
+    bmBurgerButton: {
+      position: 'fixed',
+      width: '40px',
+      height: '40px',
+      top: '90px'
+    },
+    bmBurgerBars: {
+      background: '#373a47'
+    },
+    bmCrossButton: {
+      height: '24px',
+      width: '24px',
+    },
+    bmCross: {
+      background: '#bdc3c7'
+    },
+    bmMenu: {
+      background: '#373a47',
+      fontSize: '1.15em',
+      height: "100%",
+      width: "100%",
+    },
+    bmMorphShape: {
+      fill: '#373a47'
+    },
+    bmItemList: {
+      color: '#b8b7ad',
+      padding: '0.8em'
+    },
+    bmOverlay: {
+      background: 'rgba(0, 0, 0, 0.3)'
+    }
+  }
 export default connect(mapStateToProps, mapDispatchToProps)(InstructorSettings);
