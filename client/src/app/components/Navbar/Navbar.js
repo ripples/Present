bc2ba399cd3e59791b0085f28b318217afa23de8
Rajesh Susai <rajeshsusai14@gmail.com>
2018-04-07 11:09:48 -0400

    resizing button to fit text

--- client/src/app/components/Navbar/Navbar.js
+++ client/src/app/components/Navbar/Navbar.js
@@ -9,23 +9,23 @@ class Navbar extends Component {
         return(
             <div className="navbar navbar-fixed-top navbar-default" style={barStyle}>
                 <div className="container-fluid">
-                    <div className="col-md-5">
-                        <Link to="/">
-                            <button className="pull-left" style={buttonStyle}>
-                                <h1 style={headerStyle}>Lecture Viewer 2</h1>
-                            </button>
-                        </Link>
-                    </div>
-                    <div className="col-md-4" >
-                    </div>
-                    <div className="col-md-3">
-                    {
-                        ((typeof(this.props.roles) !== "undefined" && this.props.roles.toLowerCase().includes("instructor")) ?
-                            <Link to={"/course/" + this.props.courseId + "/instructorSettings"}>
-                                <button style={instructorButtonStyle}>Settings &#9881;</button>
-                            </Link>
-                            : null )
-                    }
+                    <div className = "row">
+                        <div className="col-md-12">
+                            <div className = "pull-left">
+                                <Link to="/">
+                                    <button style={buttonStyle}>Lecture Viewer 2</button>
+                                </Link>
+                            </div>
+                            <div className = "pull-right">
+                            {
+                                ((typeof(this.props.roles) !== "undefined" && this.props.roles.toLowerCase().includes("instructor")) ?
+                                    <Link to={"/course/" + this.props.courseId + "/instructorSettings"}>
+                                        <button style={instructorButtonStyle}>Settings &#9881;</button>
+                                    </Link>
+                                    : null )
+                            }
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -46,10 +46,12 @@ var barStyle = {
 }
 
 var buttonStyle= {
+    fontWeight: "bold",
+    color: "white",
+    fontSize: "38px",
     backgroundColor: "#0074D9",
     border: "none",
-    outline: "none",
-    fontWeight: "bold"
+    outline: "none"
 }
 
 var headerStyle= {
@@ -64,7 +66,7 @@ var instructorButtonStyle= {
     backgroundColor: "#0074D9",
     border: "none",
     outline: "none",
-    marginTop: "20px"
+    marginTop: "11px"
 }
 
 export default connect(mapStateToProps)(Navbar);
